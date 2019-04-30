1dd48a2ce261535fa20582987a4bfa9359ef385e
Nico De Witte <nico.dewitte@vives.be>
2019-04-30 23:18:24 +0200

    Add basic login screen for user

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'screens/controller_screen.dart';
 import 'screens/loading_screen.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'BUG Mobile Game Controller',
-    initialRoute: '/loading',
+    initialRoute: '/login',
     routes: {
       '/loading': (context) => LoadingScreen(),
+      '/login': (context) => LoginScreen(),
       '/': (context) => MyHomePage()
     }
   ));
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
+import '../bug/user.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -10,61 +11,74 @@ class _LoginScreenState extends State<LoginScreen> {
   final userIdController = TextEditingController(text: '');
   final userNameController = TextEditingController(text: '');
 
+  final key = new GlobalKey<ScaffoldState>();   // Required for Toast (Snackbar Messages)
+
   @override
   Widget build(BuildContext context) {
-    // final _formKey = GlobalKey<FormState>();
     return Scaffold(
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 45),
-                child: Text(
-                  'Please supply your User ID and Name below or scan the QR code from your profile on the Kiosk.',
-                  style: TextStyle(fontWeight: FontWeight.bold), textScaleFactor: 1.4,
-                ),
-              ),
-              SizedBox(height: 35),
-              RaisedButton(
-                child: Text('Scan QR Code'),
-                textColor: Colors.white,
-                color: Colors.redAccent,
-                onPressed: () {
-                  print("User pressed QR code button");
-                },
-              ),
-              SizedBox(height: 35),
-              ListTile(
-                leading: const Icon(Icons.videogame_asset),
-                title: TextField(
-                  controller: userIdController,
-                  decoration: InputDecoration(
-                    hintText: "User ID ...",
+      key: key,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 45),
+                  child: Text(
+                    'Please supply your User ID and Name below or scan the QR code from your profile on the Kiosk.',
+                    style: TextStyle(fontWeight: FontWeight.bold), textScaleFactor: 1.4,
                   ),
                 ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.face),
-                title: TextField(
-                  controller: userNameController,
-                  decoration: InputDecoration(
-                    hintText: "Name ...",
+                SizedBox(height: 35),
+                RaisedButton(
+                  child: Text('Scan QR Code'),
+                  textColor: Colors.white,
+                  color: Colors.redAccent,
+                  onPressed: () {
+                    print("User pressed QR code button");
+                  },
+                ),
+                SizedBox(height: 35),
+                ListTile(
+                  leading: const Icon(Icons.videogame_asset),
+                  title: TextField(
+                    controller: userIdController,
+                    decoration: InputDecoration(
+                      hintText: "User ID ...",
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 35),
-              RaisedButton(
-                child: Text('Continue'),
-                textColor: Colors.white,
-                color: Colors.redAccent,
-                onPressed: () {
-                  print("User is logging in");
-                },
-              ),
-            ],
+                ListTile(
+                  leading: const Icon(Icons.face),
+                  title: TextField(
+                    controller: userNameController,
+                    decoration: InputDecoration(
+                      hintText: "Name ...",
+                    ),
+                  ),
+                ),
+                SizedBox(height: 35),
+                RaisedButton(
+                  child: Text('Continue'),
+                  textColor: Colors.white,
+                  color: Colors.redAccent,
+                  onPressed: () {
+                    if (userNameController.text?.isNotEmpty
+                      && userIdController.text?.isNotEmpty) {
+                        User user = new User(userIdController.text, userNameController.text);
+                        print("Logging in ...");
+                        Navigator.pushNamed(context, '/');
+                    } else {
+                      key.currentState.showSnackBar(new SnackBar(
+                        content: const Text("Please enter ID and Name")
+                      ));
+                    }
+                  },
+                ),
+              ],
+            )
           )
         )
       )
