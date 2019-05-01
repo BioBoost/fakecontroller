d810242f48183e3cac0fe3b0c137dbe736c9dcac
Nico De Witte <nico.dewitte@vives.be>
2019-05-01 15:14:31 +0200

    Add builder for our mqtt client based on stored settings

---
+++ lib/bug/mqtt_builder.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:bug_mobile_controller/bug/simple_mqtt_client.dart';
+import 'package:bug_mobile_controller/settings/mqtt_settings_storage.dart';
+
+class MqttSimpleClientBuilder {
+  static Future<SimpleMqttClient> create() async {
+    // Load MQTT settings
+    MqttSettingsStorage.load()
+    .then((settings) {
+      print("Settings loaded succesfully: " + settings.toString());
+      settings.broker = "api.bug.labict.be/broker";
+      settings.useWebSockets = true;
+      return new SimpleMqttClient(settings);
+    })
+    .catchError((onError) {
+      print("Failed to load settings. Reverting to default");
+    });
+    return null;
+  }
+}
