dd04a7bba971622bedbdaf2614db1c52b1f27833
Nico De Witte <nico.dewitte@vives.be>
2019-05-02 10:59:06 +0200

    Refactor addon selection code

--- lib/screens/controller_screen.dart
+++ lib/screens/controller_screen.dart
@@ -86,149 +86,21 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  String dropdownAddOnValue1 = "Add on";
-  String dropdownAddOnValue2 = "Add on";
-  String dropdownAddOnValue3 = "Add on";
-  String rocketEngineAdd = "01a2f560d6df03bb";
-  String amphibiousAdd = "0140c29c8357f2ce"; //-> 0140c29c8357f2ce
-  String harrierAdd = "0155cf8199f0245b"; //-> 0155cf8199f0245b
-  String adamantiumAdd = "016bc4464286f3fb"; //-> 016bc4464286f3fb
-  String gravyShieldAdd = "0148eef363dff533"; //-> 0148eef363dff533
-  String nanobotsAdd = "0164a54798d7d27a"; //-> 0164a54798d7d27a
-  String structuralStrengtheningAdd = "0136edcaaf285d1d"; //-> 0136edcaaf285d1d
-  String FlammenwerpferAdd = "011d5ba90ce241e6"; //-> 011d5ba90ce241e6
-  String laserAdd = "01a2eb344edc7c5a"; //-> 01a2eb344edc7c5a
-  String minesAdd = "01d9643e2bf10134"; //-> 01d9643e2bf10134
-  String plasmaGunAdd = "0100548e2b3038f5"; //-> 0100548e2b3038f5
-  String empBombAdd = "01ff7ab8c2155e57"; //-> 01ff7ab8c2155e57
-  String ramAdd = "0122e76e424f7c79"; //-> 0122e76e424f7c79
-  String gatlingGunAdd = "01f94b5e5d4277b5"; //-> 01f94b5e5d4277b5
-
   String idHardware = "0080d0803b102f01";
-  String add_1 = "0";
-  String add_2 = "0";
-  String add_3 = "0";
-
-  void addHascodeAddons(String dropdownAddOnValue, int addOnSelection) {
-    String addOnSelectionToJsonTemp = "";
-    switch (dropdownAddOnValue) {
-      case "rocketEngine":
-        {
-          addOnSelectionToJsonTemp = rocketEngineAdd;
-        }
-        break;
-      case "amphibious":
-        {
-          addOnSelectionToJsonTemp = amphibiousAdd;
-        }
-        break;
-      case "harrier":
-        {
-          addOnSelectionToJsonTemp = harrierAdd;
-        }
-        break;
-      case "adamantium":
-        {
-          addOnSelectionToJsonTemp = adamantiumAdd;
-        }
-        break;
-      case "gravyShield":
-        {
-          addOnSelectionToJsonTemp = gravyShieldAdd;
-        }
-        break;
-      case "nanobots":
-        {
-          addOnSelectionToJsonTemp = nanobotsAdd;
-        }
-        break;
-      case "structuralStrengthening":
-        {
-          addOnSelectionToJsonTemp = structuralStrengtheningAdd;
-        }
-        break;
-      case "Flammenwerpfer":
-        {
-          addOnSelectionToJsonTemp = FlammenwerpferAdd;
-        }
-        break;
-      case "laser":
-        {
-          addOnSelectionToJsonTemp = laserAdd;
-        }
-        break;
-      case "mines":
-        {
-          addOnSelectionToJsonTemp = minesAdd;
-        }
-        break;
-      case "plasmaGun":
-        {
-          addOnSelectionToJsonTemp = plasmaGunAdd;
-        }
-        break;
-      case "empBomb":
-        {
-          addOnSelectionToJsonTemp = empBombAdd;
-        }
-        break;
-      case "ram":
-        {
-          addOnSelectionToJsonTemp = ramAdd;
-        }
-        break;
-      case "gatling gun":
-        {
-          addOnSelectionToJsonTemp = gatlingGunAdd;
-        }
-        break;
-      default:
-        {
-          addOnSelectionToJsonTemp = "invalid";
-        }
-        break;
-    }
-    switch (addOnSelection) {
-      case 1:
-        {
-          add_1 = addOnSelectionToJsonTemp;
-        }
-        break;
-
-      case 2:
-        {
-          add_2 = addOnSelectionToJsonTemp;
-        }
-        break;
-
-      case 3:
-        {
-          add_3 = addOnSelectionToJsonTemp;
-        }
-        break;
-
-      default:
-        {
-          add_1 = "invalidchose";
-        }
-        break;
-    }
-  }
 
   void createJsonSendMqttStart() {
     if (_client != null && _client.isConnected()) {
       displayedString2 = '{"id":"' +
           idHardware +
           '","add_1":"' +
-          add_1 +
+          (_selectedAddons[0]?.id ?? '') +
           '","add_2":"' +
-          add_2 +
+          (_selectedAddons[1]?.id ?? '') +
           '","add_3":"' +
-          add_3 +
+          (_selectedAddons[2]?.id ?? '') +
           '","dev_id":"' +
           _user.getName() +
           '"}';
-
       _client.publish(pubHardwareTopic, displayedString2);
     }
   }
@@ -298,6 +170,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   int _page = 0;
 
+  List<Addon> _selectedAddons = new List<Addon>(3);
+
   @override
   Widget build(BuildContext context) {
   //   SystemChrome.setPreferredOrientations([
@@ -314,14 +188,17 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                AddonDropdown(_addons, (selected) => {
-                  print("Addon 1: " + selected.name)
+                AddonDropdown(_addons, (selected) {
+                  print("Addon 1: " + selected.name);
+                  _selectedAddons[0] = selected;
                 }),
-                AddonDropdown(_addons, (selected) => {
-                  print("Addon 2: " + selected.name)
+                AddonDropdown(_addons, (selected) {
+                  print("Addon 2: " + selected.name);
+                  _selectedAddons[1] = selected;
                 }),
-                AddonDropdown(_addons, (selected) => {
-                  print("Addon 3: " + selected.name)
+                AddonDropdown(_addons, (selected) {
+                  print("Addon 3: " + selected.name);
+                  _selectedAddons[2] = selected;
                 }),
               ]
             ),
