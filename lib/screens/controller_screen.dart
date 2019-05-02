d734fbbd481a5a78d0361071398a07ebb6113461
Nico De Witte <nico.dewitte@vives.be>
2019-05-02 20:30:01 +0200

    Some cleanup of unused code

--- lib/screens/controller_screen.dart
+++ lib/screens/controller_screen.dart
@@ -23,16 +23,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<Addon> _addons = AddonLoader.load();
 
-  PageController _pageController;
-  int groupValue;
-  double opacityValue = 0;
-  String displayedString = "";
-  String displayedString2 = "";
-  String displayedStringOld = "";
   static const String mqttActionTopic = 'test/ttn';
   static const String mqttHardwareTopic = 'test/hardware';
 
   Timer timer;
+
+  String action = "idle";
+  String movement = "idle";
+
+  bool startApp = true;
+
+  String devId = "WhatToTakeAsDevId";
+  List<Addon> _selectedAddons = new List<Addon>(3);
 
   @override
   void dispose() {
@@ -41,52 +43,15 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  String action = "idle";
-  String movement = "idle";
-
-  bool leftButtonState = false;
-  bool rightButtonState = false;
-  bool upButtonState = false;
-  bool downButtonState = false;
-  bool selectButtonState = false;
-  bool startButtonState = false;
-  bool YButtonState = false;
-  bool AButtonState = false;
-  bool XButtonState = false;
-  bool BButtonState = false;
-  bool startApp = true;
-
-  Color leftButtonStateC =  Colors.red;
-  Color rightButtonStateC =  Colors.red;
-  Color upButtonStateC =  Colors.red;
-  Color downButtonStateC = Colors.red;
-  Color selectButtonStateC =  Colors.red;
-  Color startButtonStateC =  Colors.red;
-  Color YButtonStateC =  Colors.red;
-  Color AButtonStateC =  Colors.red;
-  Color XButtonStateC =  Colors.red;
-  Color BButtonStateC = Colors.red;
-
-  String devId = "WhatToTakeAsDevId";
-
   void publishActions() {
-    String json = JsonActionEncoder.encode(devId, action, movement).toString();
+    String json = JsonActionEncoder.encode(devId, action, movement);
     _client?.publish(mqttActionTopic, json);
   }
 
-  String idHardware = "0080d0803b102f01";
-
   void publishInitialHardwareDescription() {
-    String json = JsonHardwareDescriptionEncoder.encode(_user.getId(), _selectedAddons, devId).toString();
+    String json = JsonHardwareDescriptionEncoder.encode(_user.getId(), _selectedAddons, devId);
     _client.publish(mqttHardwareTopic, json);
   }
-
-  String messageFromMqtt = '{}';
-  Map<String, dynamic> jsonMQTT;
-
-  int _page = 0;
-
-  List<Addon> _selectedAddons = new List<Addon>(3);
 
   @override
   Widget build(BuildContext context) {
@@ -157,20 +122,8 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void navigationTapped(int page) {
-    _pageController.animateToPage(page,
-        duration: const Duration(milliseconds: 400), curve: Curves.ease);
-  }
-
-  void onPageChanged(int page) {
-    setState(() {
-      this._page = page;
-    });
-  }
-
   @override
   void initState() {
-    _pageController = PageController();
     super.initState();
     // timer = Timer.periodic(
     //     Duration(seconds: 3), (Timer t) => createJsonSendMqttButton());
